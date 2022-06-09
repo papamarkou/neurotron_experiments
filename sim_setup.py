9d42b7c5b4f681855d2edc5e87d39ac5ec5f35c5
papamarkou <theodore.papamarkou@gmail.com>
2022-06-09 08:24:35 +0100

    Cleaned up existing simulation set ups

--- sim_setup.py
+++ sim_setup.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 output_path = Path().joinpath('output')
 
-# %% Setup for simulation 1
+# %% Setup for simulation 1: data ~ normal(mu=0, sigma=1), varying theta_{*}
 
 num_runs = 7
 
@@ -25,4 +25,55 @@ sim01_setup = {
     'run_sgd' : True,
     'seed' : 1,
     'name' : 'sim01'
+}
+
+# %% Setup for simulation 2: data ~ normal(mu=0, sigma=3), varying theta_{*}
+
+sim02_setup = {
+    'sample_data' : lambda s : np.random.normal(loc=0.0, scale=3.0, size=s),
+    'filterlist' : [25], # r: filter size
+    'dlist' : [50], # n: input dimension
+    'boundlist' : [0, 0.125, 0.25, 0.5, 1., 2., 4.], # theta_{*}
+    'betalist' : [0.5], # beta
+    'etalist' : [0.0001], # eta: learning rate
+    'blist' : [16], # b
+    'width' : 10, # k: width
+    'num_iters' : 40000,
+    'run_sgd' : True,
+    'seed' : 2,
+    'name' : 'sim02'
+}
+
+# %% Setup for simulation 3: data ~ Laplace(loc=0, scale=2), varying theta_{*}
+
+sim03_setup = {
+    'sample_data' : lambda s : np.random.laplace(loc=0.0, scale=2.0, size=s),
+    'filterlist' : [25], # r: filter size
+    'dlist' : [50], # n: input dimension
+    'boundlist' : [0, 0.125, 0.25, 0.5, 1., 2., 4.], # theta_{*}
+    'betalist' : [0.5], # beta
+    'etalist' : [0.0001], # eta: learning rate
+    'blist' : [16], # b
+    'width' : 10, # k: width
+    'num_iters' : 40000,
+    'run_sgd' : True,
+    'seed' : 3,
+    'name' : 'sim03'
+}
+
+# %% Setup for simulation 4: data ~ student(df=4), varying theta_{*}
+
+sim04_setup = {
+    'sample_data' : lambda s : np.random.standard_t(4., size=s),
+    'filterlist' : [25], # r: filter size
+    'dlist' : [100], # n: input dimension
+    'boundlist' : [0, 0.125, 0.25, 0.5, 1., 2., 4.], # theta_{*}
+    'betalist' : [0.5], # beta
+    'etalist' : [0.0001], # eta: learning rate
+    'blist' : [16], # b
+    'width' : 10, # k: width
+    'num_iters' : 40000,
+    'run_sgd' : True,
+    'seed' : 4,
+    'name' : 'sim04'
 }
